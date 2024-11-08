406839a2eb86c18c34f76f2e37977db1b0d6118a
あああ <lanur622@gmail.com>
2024-11-08 15:50:41 +0900

    just house keeping/cleaning

--- scripts/build_tilemap.gd
+++ scripts/build_tilemap.gd
@@ -4,11 +4,11 @@ extends TileMapLayer
 # start focusing on how the Scene implicitly runs these scripts when you use them as Resources
 
 func _ready():
-	var tile_set_source = tile_set.get_source(0)
+	var tile_set_source = self.tile_set.get_source(0)
 	var map_width = 8
 	var map_height = 8
 	for y in range(map_height):
 		for x in range(map_width):
 			var random_tile_id = randi() % 13
-			set_cell(Vector2i(x, y), 0, tile_set_source.get_tile_id(random_tile_id))
+			self.set_cell(Vector2i(x, y), 0, tile_set_source.get_tile_id(random_tile_id))
 			print("Set tile at ", tile_set_source.get_tile_id(random_tile_id), " with tile ID ", random_tile_id)
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,16 +1,11 @@
 extends CharacterBody2D
 
-const SPEED = 200  # Movement speed in pixels per second
+const SPEED = 200 # Movement speed in pixels per second
 
 func _physics_process(delta):
-	var input_vector = Vector2(
-		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-	)
-
-	if input_vector != Vector2.ZERO:
-		input_vector = input_vector.normalized()
-		print("Input detected: ", input_vector)
-
-	velocity = input_vector * SPEED
+	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	# TODO: delta here is often too low as the time for frame draw 
+	# just writing self to make it clear im in OOP
+	self.velocity = input_dir * SPEED # * delta
+	print("fps: ", 1 / delta)
 	move_and_slide()
