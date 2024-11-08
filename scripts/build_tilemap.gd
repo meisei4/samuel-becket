cd489331085fb579d0c276c96132f6a01f099226
あああ <lanur622@gmail.com>
2024-11-08 15:31:52 +0900

    format second

--- scripts/build_tilemap.gd
+++ scripts/build_tilemap.gd
@@ -10,5 +10,5 @@ func _ready():
 	for y in range(map_height):
 		for x in range(map_width):
 			var random_tile_id = randi() % 13
-			set_cell(Vector2i(x,y), 0, tile_set_source.get_tile_id(random_tile_id))
+			set_cell(Vector2i(x, y), 0, tile_set_source.get_tile_id(random_tile_id))
 			print("Set tile at ", tile_set_source.get_tile_id(random_tile_id), " with tile ID ", random_tile_id)
